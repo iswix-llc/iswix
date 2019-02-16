f38ef8dcfc5cb3b16d3085f0e12c438ed0abb0ec
Christopher Painter <chrpai@iswix.com>
2019-02-16 10:45:08 -0600

    MVVM work

---
+++ Source/Application/Designers/GeneralInformationDesigner/Models/ProductModel.cs
@@ -0,0 +1,44 @@
+﻿using FireworksFramework.Types;
+using System;
+using System.ComponentModel;
+
+namespace Designers.GeneralInformation.Models
+{
+    public class ProductModel : ObservableObject
+    {
+        string _id;
+        [CategoryAttribute("Product")]
+        [Description(@"The product code GUID for the product.")]
+        public string Id { get { return _id; } set { _id = value; RaisePropertyChangedEvent("Id"); } }
+
+        string _codePage;
+        [CategoryAttribute("Product")]
+        [Description(@"The code page integer value or web name for the resulting MSI.")]
+        public string Codepage { get { return _codePage; } set { _codePage = value; RaisePropertyChangedEvent("CodePage"); } }
+
+        Int32 _language;
+        [CategoryAttribute("Product")]
+        [Description("The decimal language ID (LCID) of the merge module.")]
+        public Int32 Language { get { return _language; } set { _language = value; RaisePropertyChangedEvent("Language"); } }
+
+        string _manufacturer;
+        [CategoryAttribute("Product")]
+        [Description(@"The manufacturer of the product.")]
+        public string Manufacturer { get { return _manufacturer; } set { _manufacturer = value; RaisePropertyChangedEvent("Manufacturer"); } }
+
+        string _name;
+        [CategoryAttribute("Product")]
+        [Description(@"The descriptive name of the product.")]
+        public string Name { get { return _name; } set { _name = value; RaisePropertyChangedEvent("Name"); }}
+
+        string _upgradeCode;
+        [CategoryAttribute("Product")]
+        [Description(@"The upgrade code GUID for the product.")]
+        public string UpgradeCode { get { return _upgradeCode; } set { _upgradeCode = value; RaisePropertyChangedEvent("UpgradeCode"); } }
+
+        string _version;
+        [CategoryAttribute("Product")]
+        [Description(@"The product's version string.")]
+        public string Version { get { return _version; } set { _version = value; RaisePropertyChangedEvent("Version"); } }
+    }
+}
--- Source/Application/Designers/GeneralInformationDesigner/ViewModels/GeneralInformationViewModel.cs
+++ Source/Application/Designers/GeneralInformationDesigner/ViewModels/GeneralInformationViewModel.cs
@@ -1,14 +1,11 @@
-﻿using Designers.GeneralInformation;
+﻿using System.Xml.Linq;
+using System.Windows;
+using IsWiXAutomationInterface;
+using Designers.GeneralInformation.Models;
 using FireworksFramework.Managers;
 using FireworksFramework.Types;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Xml.Linq;
-using IsWiXAutomationInterface;
-using System.Windows;
+using System.ComponentModel;
 
 namespace GeneralInformationDesigner.ViewModels
 {
@@ -18,17 +15,43 @@ namespace GeneralInformationDesigner.ViewModels
         DocumentManager _documentManager = DocumentManager.DocumentManagerInstance;
         XNamespace ns;
 
-        Product _product;
-        Module _module;
-        Package _package;
+        ProductModel _product;
+        ModuleModel _module;
+        PackageModel _package;
+        IsWiXProduct _iswixProduct;
+        IsWiXModule _iswixModule;
+        IsWiXPackage _iswixPackage;
+
         Visibility _productPropertyGridVisible = Visibility.Hidden;
         Visibility _modulePropertyGridVisible = Visibility.Hidden;
         Visibility _packagePropertyGridVisible = Visibility.Hidden;
-        public Product Product { get { return _product; } set { _product = value; RaisePropertyChangedEvent("Product"); } }
-        public Module Module { get { return _module; } set { _module = value; RaisePropertyChangedEvent("Module"); } }
-        public Package Package { get { return _package; } set { _package = value; RaisePropertyChangedEvent("Package"); } }
+        public ProductModel Product { get { return _product; } set { _product = value; RaisePropertyChangedEvent("Product"); } }
+        public ModuleModel Module
+        {
+            get
+            {
+                return _module;
+            }
+            set
+            {
+                _module = value;
+               RaisePropertyChangedEvent("Module");
+            }
+        }
+        public PackageModel Package { get { return _package; } set { _package = value; RaisePropertyChangedEvent("Package"); } }
         public Visibility ProductPropertyGridVisible { get { return _productPropertyGridVisible; } set { _productPropertyGridVisible = value; RaisePropertyChangedEvent("ProductPropertyGridVisible"); } }
-        public Visibility ModulePropertyGridVisible { get { return _modulePropertyGridVisible; } set { _modulePropertyGridVisible = value; RaisePropertyChangedEvent("ModulePropertyGridVisible"); } }
+        public Visibility ModulePropertyGridVisible
+        {
+            get
+            {
+                return _modulePropertyGridVisible;
+            }
+            set
+            {
+                _modulePropertyGridVisible = value;
+                RaisePropertyChangedEvent("ModulePropertyGridVisible");
+            }
+        }
         public Visibility PackagePropertyGridVisible { get { return _packagePropertyGridVisible; } set { _packagePropertyGridVisible = value; RaisePropertyChangedEvent("PackagePropertyGridVisible"); } }
 
         public void Load()
@@ -40,28 +63,197 @@ namespace GeneralInformationDesigner.ViewModels
                 case IsWiXDocumentType.Product:
                     ProductPropertyGridVisible = Visibility.Visible;
                     ModulePropertyGridVisible = Visibility.Hidden;
-                    Product product = new Product();
-                    product.Read(new IsWiXProduct());
-                    Product = product;
+                    Product = LoadProduct();
                     break;
 
                 case IsWiXDocumentType.Module:
                     ProductPropertyGridVisible = Visibility.Hidden;
                     ModulePropertyGridVisible = Visibility.Visible;
-                    Module module = new Module();
-                    module.Read(new IsWiXModule());
-                    Module = module;
+                    Module = LoadModule();
                     break;
-                    
 
                 default:
                     break;
             }
 
-            Package package = new Package();
-            package.Read(new IsWiXPackage());
-            Package = package;
+            PackageModel package = new PackageModel();
+            Package = LoadPackage();
             PackagePropertyGridVisible = Visibility.Visible;
+        }
+
+        ModuleModel LoadModule()
+        {
+            ModuleModel module = new ModuleModel();
+            _iswixModule = new IsWiXModule();
+            module.Id = _iswixModule.Id;
+            module.Language = _iswixModule.Language;
+            module.Codepage = _iswixModule.Codepage;
+            module.Version = _iswixModule.Version;
+            module.PropertyChanged += Module_PropertyChanged;
+            return module;
+        }
+
+        PackageModel LoadPackage()
+        {
+            PackageModel package = new PackageModel();
+            _iswixPackage = new IsWiXPackage();
+            package.AdminImage = _iswixPackage.AdminImage;
+            package.Comments = _iswixPackage.Comments;
+            package.Compressed = _iswixPackage.Compressed;
+            package.Description = _iswixPackage.Description;
+            package.Id = _iswixPackage.Id;
+            package.InstallerVersion = _iswixPackage.InstallerVersion;
+            package.InstallPrivileges = _iswixPackage.InstallPrivileges;
+            package.InstallScope = _iswixPackage.InstallScope;
+            package.Keywords = _iswixPackage.Keywords;
+            package.Languages = _iswixPackage.Languages;
+            package.Manufacturer = _iswixPackage.Manufacturer;
+            package.Platform = _iswixPackage.Platform;
+            package.ReadOnly = _iswixPackage.ReadOnly;
+            package.ShortNames = _iswixPackage.ShortNames;
+            package.SummaryCodepage = _iswixPackage.SummaryCodepage;
+            package.PropertyChanged += Package_PropertyChanged;
+            return package;
+        }
+
+        ProductModel LoadProduct()
+        {
+            ProductModel product = new ProductModel();
+            _iswixProduct = new IsWiXProduct();
+            product.Codepage = _iswixProduct.Codepage;
+            product.Id = _iswixProduct.Id;
+            product.Language = _iswixProduct.Language;
+            product.Manufacturer = _iswixProduct.Manufacturer;
+            product.Name = _iswixProduct.Name;
+            product.UpgradeCode = _iswixProduct.UpgradeCode;
+            product.Version = _iswixProduct.Version;
+            product.PropertyChanged += Product_PropertyChanged;
+            return product;
+        }
+
+        private void Module_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Codepage":
+                    _iswixModule.Codepage = Module.Codepage;
+                    break;
+
+                case "Id":
+                    _iswixModule.Id = Module.Id;
+                    break;
+
+                case "Language":
+                    _iswixModule.Language = Module.Language;
+                    break;
+
+                case "Version":
+                    _iswixModule.Version = Module.Version;
+                    break;
+
+            }
+
+        }
+        private void Package_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "AdminImage":
+                    _iswixPackage.AdminImage = Package.AdminImage;
+                    break;
+
+                case "Comments":
+                    _iswixPackage.Comments = Package.Comments;
+                    break;
+
+                case "Compressed":
+                    if (Package.Compressed != null && _documentManager.Document.GetDocumentType() == IsWiXDocumentType.Module)
+                    {
+                        MessageBox.Show("The compressed attribute cannot be set in a Merge Module.");
+                        Package.Compressed = null;
+                    }
+                    else
+                    {
+                        _iswixPackage.Compressed = Package.Compressed;
+                    }
+                    break;
+
+                case "Description":
+                    _iswixPackage.Description = Package.Description;
+                    break;
+
+                case "InstallPrivileges":
+                    _iswixPackage.InstallPrivileges = Package.InstallPrivileges;
+                    break;
+
+                case "ReadOnly":
+                    _iswixPackage.ReadOnly = Package.ReadOnly;
+                    break;
+
+                case "Keywords":
+                    _iswixPackage.Keywords = Package.Keywords;
+                    break;
+
+                case "Languages":
+                    _iswixPackage.Languages = Package.Languages;
+                    break;
+
+                case "InstallScope":
+                    _iswixPackage.InstallScope = Package.InstallScope;
+                    break;
+
+                case "Platform":
+                    _iswixPackage.Platform = Package.Platform;
+                    break;
+
+                case "ShortNames":
+
+                    _iswixPackage.ShortNames = Package.ShortNames;
+                    break;
+
+                case "SummaryCodepage":
+                    _iswixPackage.SummaryCodepage = Package.SummaryCodepage;
+                    break;
+
+                case "Manufacturer":
+                    _iswixPackage.Manufacturer = Package.Manufacturer;
+                    break;
+
+                case "Id":
+                    _iswixPackage.Id = Package.Id;
+                    break;
+
+                case "InstallerVersion":
+                    _iswixPackage.InstallerVersion = Package.InstallerVersion;
+                    break;
+            }
+        }
+        private void Product_PropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            switch (e.PropertyName)
+            {
+                case "Id":
+                    _iswixProduct.Id = Product.Id;
+                    break;
+                case "Codepage":
+                    _iswixProduct.Codepage = Product.Codepage;
+                    break;
+                case "Language":
+                    _iswixProduct.Language = Product.Language;
+                    break;
+                case "Manufacturer":
+                    _iswixProduct.Manufacturer = Product.Manufacturer;
+                    break;
+                case "Name":
+                    _iswixProduct.Name = Product.Name;
+                    break;
+                case "UpgradeCode":
+                    _iswixProduct.UpgradeCode = Product.UpgradeCode;
+                    break;
+                case "Version":
+                    _iswixProduct.Version = Product.Version;
+                    break;
+            }
         }
     }
 }
