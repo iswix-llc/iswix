57fbb70147ec8e901d56db333ac84d07af369e56
Christopher Painter <chrpai@iswix.com>
2019-02-15 21:20:49 -0600

    begin wpf work on generalinformation designer

---
+++ Source/Application/Designers/GeneralInformationDesigner/Views/GeneralInformationView.xaml.cs
@@ -0,0 +1,70 @@
+﻿using FireworksFramework.Interfaces;
+using FireworksFramework.Managers;
+using IsWiXAutomationInterface;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Controls;
+using static FireworksFramework.Types.Enums;
+
+namespace GeneralInformationDesigner.Views
+{
+    /// <summary>
+    /// Interaction logic for GeneralInformationView.xaml
+    /// </summary>
+    public partial class GeneralInformationView : UserControl, IFireworksDesigner
+    {
+        DocumentManager _documentManager = DocumentManager.DocumentManagerInstance;
+
+        public GeneralInformationView()
+        {
+            InitializeComponent();
+        }
+
+        public System.Drawing.Image PluginImage
+        {
+            get
+            {
+                return System.Drawing.Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("GeneralInformationDesigner.GeneralInformation.ico"));
+            }
+        }
+
+        public PluginType PluginType { get { return PluginType.Designer; } }
+
+        public string PluginInformation
+        {
+            get
+            {
+                return new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("GeneralInformationDesigner.License.txt")).ReadToEnd();
+            }
+        }
+
+        public string PluginName
+        {
+            get { return "General Information"; }
+        }
+
+        public string PluginOrder
+        {
+            get { return "iswix_group1_general_information2"; }
+        }
+
+        public bool IsValidContext()
+        {
+            IsWiXDocumentType docType = _documentManager.Document.GetDocumentType();
+
+            if (docType == IsWiXDocumentType.Product || docType == IsWiXDocumentType.Module)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void LoadData()
+        {
+        }
+    }
+}
