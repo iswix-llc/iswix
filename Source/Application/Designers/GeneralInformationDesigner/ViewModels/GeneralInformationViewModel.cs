7853063b9c52d51d15d8c1f2046c8e3ca5abcded
Christopher Painter <chrpai@iswix.com>
2019-02-15 22:18:10 -0600

    wpf work

--- Source/Application/Designers/GeneralInformationDesigner/ViewModels/GeneralInformationViewModel.cs
+++ Source/Application/Designers/GeneralInformationDesigner/ViewModels/GeneralInformationViewModel.cs
@@ -1,4 +1,5 @@
-﻿using FireworksFramework.Types;
+﻿using Designers.GeneralInformation;
+using FireworksFramework.Types;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +10,9 @@ namespace GeneralInformationDesigner.ViewModels
 {
     class GeneralInformationViewModel : ObservableObject
     {
+        Module _module = new Module();
+        Package _package = new Package();
+        public Module Module { get { return _module; } set { _module = value; RaisePropertyChangedEvent("Module"); } }
+        public Package Package { get { return _package; } set { _package = value; RaisePropertyChangedEvent("Package"); } }
     }
 }
