04d26bf25824018a1a44bf4fc9b99e667e6f40b2
Christopher Painter <chrpai@iswix.com>
2019-02-17 20:47:23 -0600

    Begin WPF work on PropertiesDesigner

---
+++ Application/Designers/PropertiesDesigner/ViewModels/PropertiesViewModel.cs
@@ -0,0 +1,44 @@
+﻿using FireworksFramework.Managers;
+using FireworksFramework.Types;
+using IsWiXAutomationInterface;
+using PropertiesDesigner.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PropertiesDesigner.ViewModels
+{
+    public class PropertiesViewModel : ObservableObject
+    {
+
+        DocumentManager _documentManager = DocumentManager.DocumentManagerInstance;
+        XNamespace ns;
+        IsWiXProperties _iswixProperties;
+
+        ObservableCollection<PropertyModel> _properties = new ObservableCollection<PropertyModel>();
+        public ObservableCollection<PropertyModel> Properties { get { return _properties; } set { _properties = value; RaisePropertyChangedEvent("Properties"); } }
+
+        public void Load()
+        {
+            ObservableCollection<PropertyModel> properties = new ObservableCollection<PropertyModel>();
+            _iswixProperties = new IsWiXProperties();
+
+            foreach (var iswixProperty in _iswixProperties)
+            {
+                PropertyModel propertyModel = new PropertyModel();
+                propertyModel.Id = iswixProperty.Id;
+                propertyModel.Value = iswixProperty.Value;
+                propertyModel.Admin = iswixProperty.Admin;
+                propertyModel.Hidden = iswixProperty.Hidden;
+                propertyModel.Secure = iswixProperty.Secure;
+                propertyModel.SuppressModularization = iswixProperty.SuppressModularization;
+                properties.Add(propertyModel);
+            }
+            Properties = properties;
+        }
+    }
+}
---
+++ Application/Designers/PropertiesDesigner/Views/PropertiesView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using FireworksFramework.Interfaces;
+using FireworksFramework.Managers;
+using IsWiXAutomationInterface;
+using System;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Windows.Controls;
+using static FireworksFramework.Types.Enums;
+
+namespace PropertiesDesigner.Views
+{
+    /// <summary>
+    /// Interaction logic for PropertiesView.xaml
+    /// </summary>
+    public partial class PropertiesView : UserControl, IFireworksDesigner
+    {
+
+        public PropertiesView()
+        {
+            InitializeComponent();
+        }
+
+        public System.Drawing.Image PluginImage
+        {
+            get
+            {
+                return System.Drawing.Image.FromStream(Assembly.GetExecutingAssembly().GetManifestResourceStream("PropertiesDesigner.Properties.ico"));
+            }
+        }
+
+        public string PluginInformation
+        {
+            get
+            {
+                return new StreamReader(Assembly.GetExecutingAssembly().GetManifestResourceStream("PropertiesDesigner.License.txt")).ReadToEnd();
+            }
+        }
+
+        public PluginType PluginType { get { return PluginType.Designer; } }
+
+        public string PluginName
+        {
+            get { return "Properties"; }
+        }
+
+        public string PluginOrder
+        {
+            get { return "iswix_group2_properties"; }
+        }
+        public bool IsValidContext()
+        {
+
+            if (DocumentManager.DocumentManagerInstance.Document.GetDocumentType() == IsWiXDocumentType.None)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        public void LoadData()
+        {
+            viewModel.Load();
+        }
+    }
+}
