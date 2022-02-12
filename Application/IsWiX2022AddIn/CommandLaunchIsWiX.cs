93cb01e448fecc69a768ca089904e427c45a4452
Christopher Painter <chrpai@iswix.com>
2022-02-11 22:36:32 -0600

    port vs2022 template to latest project type

---
+++ Application/IsWiX2022AddIn/CommandLaunchIsWiX.cs
@@ -0,0 +1,139 @@
+﻿
+using EnvDTE;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Microsoft.Win32;
+using System;
+using System.ComponentModel.Design;
+using System.Globalization;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Task = System.Threading.Tasks.Task;
+
+namespace IsWiX2022AddIn
+{
+    /// <summary>
+    /// Command handler
+    /// </summary>
+    internal sealed class CommandLaunchIsWiX
+    {
+        /// <summary>
+        /// Command ID.
+        /// </summary>
+        public const int CommandId = 0x0100;
+
+        /// <summary>
+        /// Command menu group (command set GUID).
+        /// </summary>
+        public static readonly Guid CommandSet = new Guid("70a7dc65-5e96-47af-85ef-5a47ee574807");
+
+        /// <summary>
+        /// VS Package that provides this command, not null.
+        /// </summary>
+        private readonly AsyncPackage package;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CommandLaunchIsWiX"/> class.
+        /// Adds our command handlers for menu (commands must exist in the command table file)
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        /// <param name="commandService">Command service to add command to, not null.</param>
+        private CommandLaunchIsWiX(AsyncPackage package, OleMenuCommandService commandService)
+        {
+            this.package = package ?? throw new ArgumentNullException(nameof(package));
+            commandService = commandService ?? throw new ArgumentNullException(nameof(commandService));
+
+            var menuCommandID = new CommandID(CommandSet, CommandId);
+            var menuItem = new MenuCommand(this.Execute, menuCommandID);
+            commandService.AddCommand(menuItem);
+        }
+
+        /// <summary>
+        /// Gets the instance of the command.
+        /// </summary>
+        public static CommandLaunchIsWiX Instance
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// Gets the service provider from the owner package.
+        /// </summary>
+        private Microsoft.VisualStudio.Shell.IAsyncServiceProvider ServiceProvider
+        {
+            get
+            {
+                return this.package;
+            }
+        }
+
+        /// <summary>
+        /// Initializes the singleton instance of the command.
+        /// </summary>
+        /// <param name="package">Owner package, not null.</param>
+        public static async Task InitializeAsync(AsyncPackage package)
+        {
+            // Switch to the main thread - the call to AddCommand in CommandLaunchIsWiX's constructor requires
+            // the UI thread.
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync(package.DisposalToken);
+
+            OleMenuCommandService commandService = await package.GetServiceAsync(typeof(IMenuCommandService)) as OleMenuCommandService;
+            Instance = new CommandLaunchIsWiX(package, commandService);
+        }
+
+        /// <summary>
+        /// This function is the callback used to execute the command when the menu item is clicked.
+        /// See the constructor to see how the menu item is associated with this function using
+        /// OleMenuCommandService service and MenuCommand class.
+        /// </summary>
+        /// <param name="sender">Event sender.</param>
+        /// <param name="e">Event args.</param>
+        private void Execute(object sender, EventArgs e)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            try
+            {
+
+                DTE dte = Package.GetGlobalService(typeof(DTE)) as DTE;
+
+                string documentPath;
+                if (dte.ActiveDocument == null)
+                {
+                    documentPath = string.Empty;
+                }
+                else
+                {
+                    documentPath = "\"" + dte.ActiveDocument.FullName + "\"";
+                }
+                string isWiXPath = string.Empty; ;
+                using (RegistryKey local32Key = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, RegistryView.Registry32))
+                {
+                    using (RegistryKey isWiXKey = local32Key.OpenSubKey(@"SOFTWARE\ISWIXLLC\IsWiX", false))
+                    {
+                        isWiXPath = isWiXKey.GetValue("IsWiXFilePath", string.Empty).ToString();
+                    }
+                }
+
+                if (!File.Exists(isWiXPath))
+                {
+                    throw new Exception("IsWiX.exe not found");
+                }
+                System.Diagnostics.Process.Start(isWiXPath, documentPath);
+            }
+            catch (Exception ex)
+            {
+                VsShellUtilities.ShowMessageBox(
+                    this.package,
+                    $"IsWiX Failed to launch. Reason:\r\n{ex.Message}",
+                    "IsWiX",
+                    OLEMSGICON.OLEMSGICON_INFO,
+                    OLEMSGBUTTON.OLEMSGBUTTON_OK,
+                    OLEMSGDEFBUTTON.OLEMSGDEFBUTTON_FIRST);
+            }
+        }
+    }
+}
+
