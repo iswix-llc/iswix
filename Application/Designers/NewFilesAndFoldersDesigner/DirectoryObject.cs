0c56e347b29e67766871aaa8a3a995d3e29c6346
Painter, Christopher P <CHRISTOPHER_P_PAINTER@homedepot.com>
2023-04-14 21:35:21 -0500

    clone Files and Folders designer for WixLib work

---
+++ Application/Designers/NewFilesAndFoldersDesigner/DirectoryObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+
+namespace Designers.NewFilesAndFolders
+{
+    class DirectoryObject
+    {
+        [CategoryAttribute("Directory")]
+        public string Id { get; set; }
+        [Category("Directory")]
+        public string Name { get; set; }
+
+        public void Read(XElement document)
+        {
+            IEnumerable<XElement> directories = 
+                from dir in document.Elements("Directory")
+                select dir;
+
+            foreach (XElement directory in directories)
+            {
+                Id = (directory.Attribute("Id") != null) ? directory.Attribute("Id").Value : null;
+                Name = (directory.Attribute("Name") != null) ? directory.Attribute("Name").Value : null;
+            }
+        }
+
+
+    
+    }
+}
